5a517df98f3373ab88124d117ddbd427b163c7c7
Oliver Faderbauer <ofaderbauer@gmail.com>
2015-02-17 22:38:04 +0100

    stuff

---
+++ app/src/main/java/eu/isawsm/accelerate/ax/MainActivity.java
@@ -0,0 +1,71 @@
+package eu.isawsm.accelerate.ax;
+
+
+
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
+
+import eu.isawsm.accelerate.R;
+
+public class MainActivity extends ActionBarActivity {
+
+    private Toolbar toolbar;
+
+    private RecyclerView mRecyclerView;
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ax_recycler);
+
+        toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+
+        mRecyclerView = (RecyclerView)findViewById(R.id.ax_recycler_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+
+        Window window = getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.setStatusBarColor(getResources().getColor(R.color.colorPrimaryDark));
+
+//
+//        mAdapter = new axAdapter(CountryManager.getInstance().getCountries(), R.layout.row_country, this);
+//        mRecyclerView.setAdapter(mAdapter);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/eu/isawsm/accelerate/ax/axAdapter.java
@@ -0,0 +1,27 @@
+package eu.isawsm.accelerate.ax;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by ofade_000 on 12.02.2015.
+ */
+public class axAdapter extends RecyclerView.Adapter {
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder viewHolder, int i) {
+//        View v = LayoutInflater.from(viewHolder.getContext()).inflate(rowLayout, viewGroup, false);
+//        return new RecyclerView.ViewHolder(v);
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
