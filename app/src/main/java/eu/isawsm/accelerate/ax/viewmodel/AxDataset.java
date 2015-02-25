c80598d24bc607b90fce6d36c2ab4ef3b720927e
Oliver Faderbauer <ofaderbauer@gmail.com>
2015-02-25 17:19:24 +0100

    homeoffice checkin

--- app/src/main/java/eu/isawsm/accelerate/ax/viewmodel/AxDataset.java
+++ app/src/main/java/eu/isawsm/accelerate/ax/viewmodel/AxDataset.java
@@ -1,60 +1,63 @@
 package eu.isawsm.accelerate.ax.viewmodel;
 
-import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
 
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
+
+import eu.isawsm.accelerate.ax.AxAdapter;
 
 /**
  * Created by olfad on 24.02.2015.
  */
-public class AxDataset extends ArrayList {
+public class AxDataset<T> extends ArrayList<T> {
 
-    @Override
-    public void add(int index, Object object) {
-        super.add(index, object);
+    private AxAdapter adapter;
+
+    public AxDataset(AxAdapter adapter){
+        super();
+        this.adapter = adapter;
     }
 
     @Override
-    public boolean add(Object object) {
-        return super.add(object);
+    public void add(int index, T t) {
+        super.add(index, t);
+        adapter.notifyItemInserted(index);
+        adapter.notifyItemRangeChanged(index, size());
+    }
+
+    @Override
+    public boolean add(T t) {
+        super.add(t);
+        adapter.notifyItemInserted(size()-1);
+        return true;
     }
 
     @Override
     public void clear() {
         super.clear();
+        adapter.notifyItemRangeRemoved(0, size());
+        adapter.notifyItemRangeChanged(0, size());
     }
 
     @Override
-    public Object set(int index, Object object) {
-        return super.set(index, object);
+    public T set(int index, T t) {
+        T prevItem = super.set(index, t);
+            adapter.notifyItemChanged(index);
+        return prevItem;
     }
 
     @Override
     public boolean remove(Object object) {
-        return super.remove(object);
+        boolean listModified = super.remove(object);
+        adapter.notifyItemRemoved(indexOf(object));
+        adapter.notifyDataSetChanged();
+        return listModified;
     }
 
     @Override
-    public Object remove(int index) {
-        return super.remove(index);
-    }
-
-    @Override
-    public boolean contains(Object object) {
-        return super.contains(object);
-    }
-
-    @Override
-    public int size() {
-        return super.size();
-    }
-
-    @Override
-    public Object get(int index) {
-        return super.get(index);
+    public T remove(int index) {
+        T removedObject = super.remove(index);
+        adapter.notifyItemRemoved(index);
+        return removedObject;
     }
 }
